968b23b6ca2698b422b900a473adb68015b9fcb2
Singlemai <422107224@qq.com>
2026-07-07 11:38:34 +0800

    refactor(issue-manager): remove unused reference picker shim
    Signed-off-by: Singlemai <422107224@qq.com>

--- packages/workspace/issue-manager/src/ui/react/internal/reference/useIssueManagerReferencePickerView.ts
+++
@@ -1,5 +0,0 @@
-export {
-  useWorkspaceFileReferencePickerView as useIssueManagerReferencePickerView,
-  type UseWorkspaceFileReferencePickerViewInput as UseIssueManagerReferencePickerViewInput,
-  type WorkspaceFileReferencePreviewState as IssueManagerReferencePreviewState
-} from "@tutti-os/workspace-file-reference/react";
