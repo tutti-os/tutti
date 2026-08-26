12d6519bb4b86d9b9af566c3b9adc337a6e028cc
jomeswang <1551403343@qq.com>
2026-08-26 18:49:18 +0800

    test: omit unused fake receiver name
    Signed-off-by: jomeswang <1551403343@qq.com>

--- services/tuttid/wiring_agent_service_test.go
+++ services/tuttid/wiring_agent_service_test.go
@@ -22,7 +22,7 @@ func (s *recordingAgentAuthInvalidationSessions) InvalidateProviderRuntimeCreden
 	s.runtimeCredentials = append(s.runtimeCredentials, provider)
 }
 
-func (_ *recordingAgentAuthInvalidationSessions) SetLiveModelCatalogUpdated(func(string)) {}
+func (*recordingAgentAuthInvalidationSessions) SetLiveModelCatalogUpdated(func(string)) {}
 
 func TestStartAgentModelInvalidationAuthWatcherInvalidatesRuntimeCredentials(t *testing.T) {
 	sessions := &recordingAgentAuthInvalidationSessions{}
